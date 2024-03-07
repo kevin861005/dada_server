4bf170f3ff35f16812bd5e6f4a347bae616ddba8
Kevin Chen <kevin_chen@interinfo.com.tw>
2024-03-07 15:04:32 +0800

    first commit

---
+++ src/main/java/com/interinfo/dada_server/DadaServerApplication.java
@@ -0,0 +1,13 @@
+package com.interinfo.dada_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interinfo/dada_server/DadaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interinfo.dada_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
